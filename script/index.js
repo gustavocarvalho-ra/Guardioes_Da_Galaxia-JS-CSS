cd97bd98742abc307abc29ef08d07d6a447ce812
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-29 22:25:45 -0300

    at5

--- script/index.js
+++ script/index.js
@@ -8,12 +8,27 @@ const members = [
 ]
 
 let activeMember = 0
+const images = document.getElementById('images')
 
+function changeStatusButtons() {
+  const prev = document.getElementById('button_prev')
+  const isFirst = activeMember === 0
+  prev.disabled = isFirst
+
+  const next = document.getElementById('button_next')
+  const ifLast = activeMember === members.length - 1
+  next.disabled = ifLast
+
+}
 
 function navigationMember(direction) {
   activeMember = activeMember + direction
-  
+
   const member = members[activeMember]
 
   console.log(activeMember, member)
+
+  images.style.transform = `translateY(${-100 * activeMember}vh)`
+  
+  changeStatusButtons()
 }
